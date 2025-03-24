bfa034d2ef466423f496985128ed1e02f7570b5e
Ethan Subasic <ethansubasic@brick.ucalgary.ca>
2025-03-24 16:33:13 -0600

    update the db stuff to match the test

--- RocketControlUnitGUI/src/lib/hooks/usePocketbase.ts
+++ RocketControlUnitGUI/src/lib/hooks/usePocketbase.ts
@@ -6,8 +6,8 @@ import { currentState } from '../stores';
 export type PocketbaseHook = ReturnType<typeof usePocketbase>;
 
 export const usePocketbase = (timestamps: Timestamps, stores: Stores) => {
-	//const pocketbase = new PocketBase('http://192.168.0.69:8090');
-	const pocketbase = new PocketBase('http://127.0.0.1:8090/');
+	const pocketbase = new PocketBase('http://192.168.0.69:8090');
+	// const pocketbase = new PocketBase('http://127.0.0.1:8090/');
 
 	const authenticate = async () => {
 		const email = 'ethan.subasic@gmail.com';
@@ -57,56 +57,56 @@ export const usePocketbase = (timestamps: Timestamps, stores: Stores) => {
 
 	const subscribeToCollections = () => {
         pocketbase.collection('Plc').subscribe('*', (e) => {
-            stores.pbv1_open.set(e.record.PBV1);
-			stores.pbv2_open.set(e.record.PBV2);
-			stores.pbv3_open.set(e.record.PBV3);
-			stores.pbv4_open.set(e.record.PBV4);
-			stores.pbv5_open.set(e.record.PBV5);
-			stores.pbv6_open.set(e.record.PBV6);
-			stores.pbv7_open.set(e.record.PBV7);
-			stores.pbv8_open.set(e.record.PBV8);
-            stores.pbv9_open.set(e.record.PBV9);
-            stores.pbv10_open.set(e.record.PBV10);
-            stores.pbv11_open.set(e.record.PBV11);
+            stores.pbv1_open.set(e.record.PBV1[0]);
+			stores.pbv2_open.set(e.record.PBV2[0]);
+			stores.pbv3_open.set(e.record.PBV3[0]);
+			stores.pbv4_open.set(e.record.PBV4[0]);
+			stores.pbv5_open.set(e.record.PBV5[0]);
+			stores.pbv6_open.set(e.record.PBV6[0]);
+			stores.pbv7_open.set(e.record.PBV7[0]);
+			stores.pbv8_open.set(e.record.PBV8[0]);
+            stores.pbv9_open.set(e.record.PBV9[0]);
+            stores.pbv10_open.set(e.record.PBV10[0]);
+            stores.pbv11_open.set(e.record.PBV11[0]);
 
-            stores.sol1_open.set(e.record.SOL1);
-            stores.sol2_open.set(e.record.SOL2);
-            stores.sol3_open.set(e.record.SOL3);
-            stores.sol4_open.set(e.record.SOL4);
-            stores.sol5_open.set(e.record.SOL5);
-            stores.sol6_open.set(e.record.SOL6);
-            stores.sol7_open.set(e.record.SOL7);
-            stores.sol8_open.set(e.record.SOL8);
-            stores.sol9_open.set(e.record.SOL9);
+            stores.sol1_open.set(e.record.SOL1[0]);
+            stores.sol2_open.set(e.record.SOL2[0]);
+            stores.sol3_open.set(e.record.SOL3[0]);
+            stores.sol4_open.set(e.record.SOL4[0]);
+            stores.sol5_open.set(e.record.SOL5[0]);
+            stores.sol6_open.set(e.record.SOL6[0]);
+            stores.sol7_open.set(e.record.SOL7[0]);
+            stores.sol8_open.set(e.record.SOL8[0]);
+            stores.sol9_open.set(e.record.SOL9[0]);
 
-            stores.heater_on.set(e.record.HEATER);
+            stores.heater_on.set(e.record.HEATER[0]);
 
-            stores.pmp3_on.set(e.record.PMP3);
+            stores.pmp3_on.set(e.record.PMP3[0]);
 
-            stores.ign1_on.set(e.record.IGN1);
-            stores.ign2_on.set(e.record.IGN2);
+            stores.ign1_on.set(e.record.IGN1[0]);
+            stores.ign2_on.set(e.record.IGN2[0]);
 
-            stores.pt1_pressure.set(Math.round(e.record.PT1) * 580);
-			stores.pt2_pressure.set(Math.round(e.record.PT2) * 580);
-			stores.pt3_pressure.set(Math.round(e.record.PT3) * 580);
-			stores.pt4_pressure.set(Math.round(e.record.PT4) * 145);
-			stores.pt5_pressure.set(Math.round(e.record.PT5) * 145);
-			stores.pt13_pressure.set(Math.round(e.record.PT13) * 14.5);
-			stores.pt14_pressure.set(Math.round(e.record.PT14) * 14.5);
+            stores.pt1_pressure.set(Math.round(e.record.PT1[0]) * 580);
+			stores.pt2_pressure.set(Math.round(e.record.PT2[0]) * 580);
+			stores.pt3_pressure.set(Math.round(e.record.PT3[0]) * 580);
+			stores.pt4_pressure.set(Math.round(e.record.PT4[0]) * 145);
+			stores.pt5_pressure.set(Math.round(e.record.PT5[0]) * 145);
+			stores.pt13_pressure.set(Math.round(e.record.PT13[0]) * 14.5);
+			stores.pt14_pressure.set(Math.round(e.record.PT14[0]) * 14.5);
 
-			stores.tc1_temperature.set(e.record.TC1);
-			stores.tc2_temperature.set(e.record.TC2);
-			stores.tc3_temperature.set(e.record.TC3);
-			stores.tc4_temperature.set(e.record.TC4);
-			stores.tc5_temperature.set(e.record.TC5);
-			stores.tc6_temperature.set(e.record.TC6);
-			stores.tc7_temperature.set(e.record.TC7);
-			stores.tc8_temperature.set(e.record.TC8);
-			stores.tc9_temperature.set(e.record.TC9);
+			stores.tc1_temperature.set(e.record.TC1[0]);
+			stores.tc2_temperature.set(e.record.TC2[0]);
+			stores.tc3_temperature.set(e.record.TC3[0]);
+			stores.tc4_temperature.set(e.record.TC4[0]);
+			stores.tc5_temperature.set(e.record.TC5[0]);
+			stores.tc6_temperature.set(e.record.TC6[0]);
+			stores.tc7_temperature.set(e.record.TC7[0]);
+			stores.tc8_temperature.set(e.record.TC8[0]);
+			stores.tc9_temperature.set(e.record.TC9[0]);
 
-			stores.lc1_mass.set(e.record.LC1);
-			stores.lc2_mass.set(e.record.LC2);
-            stores.lc7_mass.set(e.record.LC7);
+			stores.lc1_mass.set(e.record.LC1[0]);
+			stores.lc2_mass.set(e.record.LC2[0]);
+            stores.lc7_mass.set(e.record.LC7[0]);
 		});
 
 		pocketbase.collection('LabJack').subscribe('*', (e) => {
@@ -115,13 +115,13 @@ export const usePocketbase = (timestamps: Timestamps, stores: Stores) => {
 			stores.lc5_mass.set(e.record.LC5[0]);
 			stores.lc6_mass.set(e.record.LC6[0]);
 
-			stores.pt6_pressure.set(Math.round(e.record.PT6[0]) * 580);
-			stores.pt7_pressure.set(Math.round(e.record.PT7[0]) * 580);
-			stores.pt8_pressure.set(Math.round(e.record.PT8[0]) * 145);
-			stores.pt9_pressure.set(Math.round(e.record.PT9[0]) * 145);
-			stores.pt10_pressure.set(Math.round(e.record.PT10[0]) * 145);
-			stores.pt11_pressure.set(Math.round(e.record.PT11[0]) * 145);
-			stores.pt12_pressure.set(Math.round(e.record.PT12[0]) * 145);
+			stores.pt6_pressure.set(Math.round(e.record.PT6[0][0]) * 580);
+			stores.pt7_pressure.set(Math.round(e.record.PT7[0][0]) * 580);
+			stores.pt8_pressure.set(Math.round(e.record.PT8[0][0]) * 145);
+			stores.pt9_pressure.set(Math.round(e.record.PT9[0][0]) * 145);
+			stores.pt10_pressure.set(Math.round(e.record.PT10[0][0]) * 145);
+			stores.pt11_pressure.set(Math.round(e.record.PT11[0][0]) * 145);
+			stores.pt12_pressure.set(Math.round(e.record.PT12[0][0]) * 145);
 		});
 
 		pocketbase.collection('SystemState').subscribe('*', (e) => {
